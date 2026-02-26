ed77fab747f7e20e6eada8d687e3c5c6b9e1bc6a
mvishwasnayak-web <mvishwasnayak@gmail.com>
2026-02-26 10:47:51 +0530

    Delete 6.ListAllFilesVerbose.ps1

--- 6.ListAllFilesVerbose.ps1
+++
@@ -1,6 +0,0 @@
-# Question 6
-
-# Write a PowerShell script to list all files in the C:\Data directory with verbose output
-# enabled.
-
-Get-ChildItem -Path "C:\AutomationLab" -Recurse -File -Verbose
