6e016478b18ef10c69c552dee2c3e99581fdb86a
mvishwasnayak-web <mvishwasnayak@gmail.com>
2026-02-26 10:48:53 +0530

    Delete 3.Top7ProcessWithHighestMemoryUse.ps1

--- 3.Top7ProcessWithHighestMemoryUse.ps1
+++
@@ -1,6 +0,0 @@
-
-Get-Process | 
-    Select-Object Name, Id, @{Name="Memory(MB)";Expression={[math]::Round($_.WorkingSet64 / 1MB, 2)}} | 
-    Sort-Object -Property "Memory(MB)" -Descending | 
-    Select-Object -First 7 | 
-    Format-Table -AutoSize
