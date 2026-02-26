d5197d4c907d1859f18e832c462d00bbe4efe075
mvishwasnayak-web <mvishwasnayak@gmail.com>
2026-02-26 10:48:28 +0530

    Delete 2.SimulateRestartingWinServices.ps1

--- 2.SimulateRestartingWinServices.ps1
+++
@@ -1,5 +0,0 @@
-Get-Service | Where-Object{ $_.Status -eq 'Stopped' } | 
-    ForEach-Object { 
-        Write-Host "Simulating restart of service: $($_.Name) with status: $($_.Status)" 
-        Restart-Service -Name $_.Name -WhatIf 
-    }
